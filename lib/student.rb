fc04eb8e960bfaee0c28a8e60271193ec36ba5cf
Whitny Edwards <wne4e2@gmail.com>
2019-02-15 00:19:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   
   def create_table
     sql = <<-SQL 
-      "CREATE TABLE students (name, grade, id) VALUES (?, ?, ?)" 
+      database_connection.execute ("CREATE TABLE students (name, grade, id) VALUES (?, ?, ?)" 
     SQL 
   end 
   
