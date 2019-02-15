236e6e496df89d209265c65534cdfcf1afa963c8
Whitny Edwards <wne4e2@gmail.com>
2019-02-15 00:15:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,18 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id 
+  end 
+  
+  def create_table
+    
+  end 
+  
+  
 end
